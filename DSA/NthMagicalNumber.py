6a48db11322482a666fd4fbb9cd44ad02445548e
Aniket Majumder <88426411+aniketryback@users.noreply.github.com>
2025-05-31 15:02:54 +0530

    Create NthMagicalNumber.py
    Bit Manipulation

---
+++ DSA/NthMagicalNumber.py
@@ -0,0 +1,40 @@
+# With brute force, not using Bit Manipulation
+import math
+def dectobin(n):
+    if n == 0:
+        return "0"
+    ans = ""
+    while(n > 0):
+        dg = n % 2
+        ans += str(dg)
+        n = n // 2
+    return ans[::-1]
+
+def magicnumber(n):
+
+    bin = reversed(dectobin(n))
+    sum = 0
+    p = 1
+    for dg in bin:
+        sum += int(dg) * math.pow(5,p)
+        p +=1
+    return int(sum)
+
+
+print(magicnumber(6))
+
+'''
+# Using Bit Manipulation
+def magicnumber2(n):
+    pow = 5
+    sum = 0
+    while(n > 0):
+        last = n & 1
+        sum += last * pow
+        n = n >> 1
+        pow = pow * 5
+    return sum
+
+print(magicnumber2(6))
+'''
+
